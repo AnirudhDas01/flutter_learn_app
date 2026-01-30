2f1dca01691bfdb5107e5da80d550e416db57758
Anirudh Das <dasanirudh5@gmail.com>
2026-01-30 16:01:07 +0530

    code update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:test_app/ui_helper/util.dart';
 
@@ -48,37 +50,102 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool isEnabled = true;
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+  bool togglePassword = true;
+  Map<String, String> data = {};
+  void printVal() {
+    if (emailController.text.trim().isNotEmpty &&
+        passwordController.text.trim().isNotEmpty) {
+      data['email'] = emailController.text;
+      data['password'] = passwordController.text;
+      String dataToJson = jsonEncode(data);
+      print(dataToJson);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Color Theme Flutter")),
+      appBar: AppBar(title: Text("Flutter User Input")),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: TextField(
-          enabled: isEnabled,
-          decoration: InputDecoration(
-            focusedBorder: OutlineInputBorder(
-              borderRadius: isEnabled
-                  ? BorderRadius.circular(50)
-                  : BorderRadius.circular(15),
-              borderSide: isEnabled
-                  ? BorderSide(color: Colors.red)
-                  : BorderSide(color: Colors.grey),
-            ),
+        child: Column(
+          spacing: 10,
+          children: [
+            TextField(
+              enabled: isEnabled,
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: isEnabled
+                      ? BorderRadius.circular(50)
+                      : BorderRadius.circular(15),
+                  borderSide: isEnabled
+                      ? BorderSide(color: Colors.red)
+                      : BorderSide(color: Colors.grey),
+                ),
 
-            disabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey),
+                disabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                prefixIcon: Icon(Icons.email),
+                labelText: "Enter Email",
+              ),
+              controller: emailController,
             ),
-            border: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.blueAccent),
-              borderRadius: BorderRadius.circular(15),
+            TextField(
+              enabled: isEnabled,
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: isEnabled
+                      ? BorderRadius.circular(50)
+                      : BorderRadius.circular(15),
+                  borderSide: isEnabled
+                      ? BorderSide(color: Colors.red)
+                      : BorderSide(color: Colors.grey),
+                ),
+
+                disabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    togglePassword = !togglePassword;
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.remove_red_eye),
+                ),
+                labelText: "Enter Password",
+              ),
+              controller: passwordController,
+              obscureText: togglePassword,
             ),
-            suffixIcon: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.remove_red_eye),
+            SizedBox(
+              width: 200,
+              height: 40,
+              child: ElevatedButton(
+                onPressed: printVal,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(3.0),
+                    side: BorderSide(color: Colors.blueAccent),
+                  ),
+                ),
+
+                child: Text("Submit"),
+              ),
             ),
-            labelText: "Enter Username",
-          ),
+          ],
         ),
       ),
     );
