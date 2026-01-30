b3d9e0d6d7f6f4adc50429d621e208a45b8013a8
Anirudh Das <dasanirudh5@gmail.com>
2026-01-30 10:50:07 +0530

    code update'

---
+++ lib/data/ui_helper/util.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+TextStyle secondaryText() {
+  return TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Colors.blueAccent,
+    fontStyle: FontStyle.italic,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:new_app/ui_helper/util.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.z
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         textTheme: TextTheme(
-          bodyMedium: TextStyle(
-            color: Colors.orangeAccent,
-            fontFamily: 'FontMain',
-            fontWeight: FontWeight.w500,
+          headlineLarge: TextStyle(
+            fontSize: 21,
+            color: Colors.orange,
+            fontWeight: FontWeight.bold,
           ),
         ),
         appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue,
           titleTextStyle: TextStyle(
+            color: Colors.white,
             fontSize: 21,
-            color: Colors.deepPurple,
-            fontFamily: 'FontMain',
             fontWeight: FontWeight.w500,
           ),
         ),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter Design and Style"),
-          backgroundColor: Colors.blue,
-        ),
-        body: Center(
-          child: Text(
-            "Hello World",
-            style: Theme.of(context).textTheme.bodyMedium,
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isEnabled = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Color Theme Flutter")),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          enabled: isEnabled,
+          decoration: InputDecoration(
+            focusedBorder: OutlineInputBorder(
+              borderRadius: isEnabled
+                  ? BorderRadius.circular(50)
+                  : BorderRadius.circular(15),
+              borderSide: isEnabled
+                  ? BorderSide(color: Colors.red)
+                  : BorderSide(color: Colors.grey),
+            ),
+
+            disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.blueAccent),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            suffixIcon: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.remove_red_eye),
+            ),
+            labelText: "Enter Username",
           ),
         ),
       ),
