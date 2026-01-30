f5768bcf82762004b9424597ad6c94fd96faa5e6
Anirudh Das <dasanirudh5@gmail.com>
2026-01-30 10:49:09 +0530

    code update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import './data/names.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,45 +11,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(
+            color: Colors.orangeAccent,
+            fontFamily: 'FontMain',
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontSize: 21,
+            color: Colors.deepPurple,
+            fontFamily: 'FontMain',
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
-            "Circle Avatar",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
-          ),
+          title: Text("Flutter Design and Style"),
           backgroundColor: Colors.blue,
         ),
-        body: ListView.separated(
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: Container(
-                width: 40,
-                height: 40,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 2, color: Colors.blue),
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.blue,
-                ),
-                child: Center(
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage('assets/images/img.png'),
-                  ),
-                ),
-              ),
-              title: Text(names[index]),
-              subtitle: Text("Last seen 20mins ago"),
-              trailing: Icon(
-                Icons.favorite,
-                color: Colors.pink,
-                size: 24.0,
-                semanticLabel: 'Text to announce in accessibility modes',
-              ),
-            );
-          },
-          itemCount: 20,
-          separatorBuilder: (BuildContext context, int index) {
-            return Divider(height: 1, thickness: 2);
-          },
+        body: Center(
+          child: Text(
+            "Hello World",
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
         ),
       ),
     );
